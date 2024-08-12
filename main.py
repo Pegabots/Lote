fa4dbac372bfd3ad7a0282a6ca2cf3d028ff8103
Otávio Gomes <otaviosantosg@gmail.com>
2024-08-12 15:56:34 -0300

    Data Scrapping Mode

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main(argv):
     res.write('handle,result,pegabot-version\n')
 
     user = open('./Dados/user_'+hashtag+'.csv', 'a')
-    user.write('created_at;default_profile;description;followers_count;friends_count;handle;lang;location;name;profile_image;twitter_id;twitter_is_protected;verified;withheld_in_countries\n')
+    user.write('created_at;description;followers_count;friends_count;handle;lang;location;name;profile_image;twitter_id;twitter_is_protected;verified;withheld_in_countries\n')
 
     timeline = open('./Dados/timeline_'+hashtag+'.csv', 'a')
     timeline.write('tweet_author;tweet_contributors;tweet_created_at;tweet_favorite_count;tweet_favorited;tweet_geo;tweet_hashtags;tweet_id;tweet_is_retweet;tweet_lang;tweet_place;tweet_retweeted;tweet_source;tweet_text\n')
@@ -25,48 +25,50 @@ def main(argv):
         spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         b = BotometerService()
         for row in spamreader:
-            try:
-                dataAnalyzed = b.catch(row[0])
-            except:
-                print("Tentando novamente")
-                dataAnalyzed = b.catch(row[0])
-
+            #try:
+            dataAnalyzed = b.catch(row[0])
+            print(dataAnalyzed)
+            #except:
+            #    print("Tentando novamente")
+            #    dataAnalyzed = b.catch(row[0])
+            analise = 'Err'
             try:
                 userData = dataAnalyzed[0]
                 timelineData = dataAnalyzed[1]
                 analise = dataAnalyzed[2]
-                user.write(str(userData[0]['created_at'])+';'+
-                            str(userData[0]['default_profile'])+';"'+
-                            str(userData[0]['description'].replace('\n', ''))+'";'+
-                            str(userData[0]['followers_count'])+';'+
-                            str(userData[0]['friends_count'])+';'+
-                            str(userData[0]['handle'])+';'+
-                            str(userData[0]['lang'])+';'+
-                            str(userData[0]['location'])+';'+
-                            str(userData[0]['name'])+';'+
-                            str(userData[0]['profile_image'])+';'+
-                            str(userData[0]['twitter_id'])+';'+
-                            str(userData[0]['twitter_is_protected'])+';'+
-                            str(userData[0]['verified'])+';'+
-                            str(userData[0]['withheld_in_countries'])+'\n')
+                user.write(str(userData['id'])+';'+
+                            str(userData['handle'])+';'+
+                            str(userData['name'])+';'+
+                            str(userData['description'].replace('\n', ''))+'";'+
+                            str(userData['created_at'])+';"'+
+                            str(userData['followers_count'])+';'+
+                            str(userData['friends_count'])+';'+
+                            str(userData['statuses_count'])+';'+
+                            str(userData['favourites_count'])+';'+
+                            str(userData['verified'])+'\n')
                 for tweet in timelineData:
-                    timeline.write(str(tweet['tweet_author'])+';'+
-                                str(tweet['tweet_contributors'])+';'+
+                    timeline.write(str(tweet['tweet_author_id'])+';'+
+                                str(tweet['tweet_author'])+';'+
                                 str(tweet['tweet_created_at'])+';'+
                                 str(tweet['tweet_favorite_count'])+';'+
-                                str(tweet['tweet_favorited'])+';'+
-                                str(tweet['tweet_geo'])+';'+
                                 str(tweet['tweet_hashtags'])+';'+
-                                str(tweet['tweet_id'])+';'+
+                                str(tweet['tweet_retweet_count'])+';'+
                                 str(tweet['tweet_is_retweet'])+';'+
-                                str(tweet['tweet_lang'])+';'+
-                                str(tweet['tweet_place'])+';'+
+                                str(tweet['tweet_is_quote'])+';'+
+                                str(tweet['tweet_is_retweet'])+';'+
+                                str(tweet['tweet_source'])+';'+
                                 str(tweet['tweet_retweeted'])+';'+
-                                str(tweet['tweet_source'])+';"'+
+                                str(tweet['in_reply_to_status_id'])+';'+
+                                str(tweet['quoted_status_text'])+';"'+
+                                str(tweet['text_user_mentions'])+';"'+
                                 str(tweet['tweet_text'].replace('\n', ''))+'"\n')
-                res.write(str(analise[0])+','+str(analise[1])+','+str(analise[2])+'\n')
+                res.write(str(analise[2])+'\n')
             except:
-                res.write(',,,'+str(dataAnalyzed)+'\n')
+                try:
+                    res.write(str(dataAnalyzed[2][0])+','+str(dataAnalyzed[2][1])+','+str(dataAnalyzed[2][2])+'\n')
+                except:
+                    print(dataAnalyzed)
+                    res.write(str(row[0])+', '+ str(dataAnalyzed)+', Err'+'\n')
             
             print(analise)
             time.sleep(2)
--- models.py
+++ models.py
@@ -4,21 +4,25 @@ import pandas as pd
 from easydict import EasyDict as edict
 
 #Importa a classe de preparação de dados
-from prepare_data import MLTools
+from prepare_data import PegaBotTools
 
 class BotProbability():
     def __init__(self):
         self.id = id
         self.total = 0
+        self.friends = 0
+        self.network = 0
+        self.temporal = 0
+        self.sentiment = 0
 
-    def predict(self, users_data, timeline_data, path_input_model="pegabot-model-01.model"):
+    def predict(self, users_data, timeline_data, path_input_model="pegabot-model-02.pbm"):
         
         #Carrega o modelo do disco
         loaded_model = pickle.load(open(path_input_model, 'rb'))
 
         #Prepara os dados do usuário para a aplicação do modelo
-        tools = MLTools()
-        x_data = tools.prepare_data(users_data, timeline_data)
+        tools = PegaBotTools()
+        x_data = tools.prepare_data(users_data, timeline_data, apply = True)
 
         #Aplica o modelo para predição e retorna a predição {[0] Não é Bot, [1] é Bot}
         #predicted = loaded_model.predict(x_data)
@@ -29,16 +33,20 @@ class BotProbability():
         return predicted_proba
 
     def botProbability(self, handle, twitterTimeline, twitterUserData):
-        try:
-            df_timeline = pd.DataFrame.from_dict(twitterTimeline)
-            df_user_data = pd.DataFrame.from_dict(twitterUserData)
-            analise = self.predict(df_user_data, df_timeline)
-            self.total = round(analise[0][1]*100, 2)
-        except:
-            self.total = -1
+        #try:
+        df_timeline = pd.DataFrame.from_dict(twitterTimeline)
+        df_user_data = pd.DataFrame.from_dict(twitterUserData)
+
+        df_timeline.to_csv("twitterTimeline", sep='\t')
+        df_user_data.to_csv("twitterUserData", sep='\t')
+
+        analise = self.predict(df_user_data, df_timeline)
+        self.total = round(analise[0][1]*100, 2)
+        #except:
+        #    self.total = -1
 
         return edict({
-            'pegabot_version': 'version-1.0.1',
+            'pegabot_version': 'version-2.0',
             'handle': handle,
             'total': self.total
         })
